8958ece8ef21d9780f27e30387e7dc0c1ac38510
Justin LeFebvre <jstnlefebvre@gmail.com>
2022-11-28 01:42:34 -0500

    reimplemented day 1 2021 in F#.

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -1,2 +1,31 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+
+let part1_single_delta depths =
+  let mutable count = 0
+  let mutable last = 10000
+  for depth in depths do
+    if depth > last then
+      count <- count + 1
+    last <- depth
+  count
+
+let part2_sliding_delta (depths: int array) =
+  let mutable count = 0
+  let mutable last = 1000000
+  for i in seq { 0 .. depths.Length - 3 } do
+    let x = depths[i]
+    let y = depths[i+1]
+    let z = depths[i+2]
+    let sum = x + y + z
+    if sum > last then
+      count <- count + 1
+    last <- sum
+  count
+
+let depths =
+  File.ReadLines "Day1/input.txt"
+  |> Seq.map int
+  |> Seq.toArray
+
+printfn "Part 1 Depth increased: %d" (part1_single_delta depths)
+printfn "Part 2 Depth increased: %d" (part2_sliding_delta depths)
