c6ca2e795841eafde06dff4cc650e463a3eb40c2
Justin LeFebvre <jstnlefebvre@gmail.com>
2022-12-02 01:03:47 -0500

    Day2 part 2

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -1,52 +1,97 @@
 ﻿open System.IO
 
-type Selection = Rock | Paper | Scissors
+type Selection = Rock = 1 | Paper = 2 | Scissors = 3
 type Throw = Selection * Selection
-
-let calculateScoreForSelection selection =
-  match selection with
-  | Rock -> 1
-  | Paper -> 2
-  | Scissors -> 3
+type MistakenGuide = Throw seq
 
 let calculateScoreForThrow (throw: Throw) =
-  let (opponent, mine) = throw
   let outcome =
-    match (opponent, mine) with
-    | (Rock, Scissors) -> 0
-    | (Rock, Paper) -> 6
-    | (Paper, Rock) -> 0
-    | (Paper, Scissors) -> 6
-    | (Scissors, Paper) -> 0
-    | (Scissors, Rock) -> 6
+    match throw with
+    | (Selection.Rock, Selection.Paper) -> 6
+    | (Selection.Paper, Selection.Scissors) -> 6
+    | (Selection.Scissors, Selection.Rock) -> 6
+    | (Selection.Rock, Selection.Scissors) -> 0
+    | (Selection.Paper, Selection.Rock) -> 0
+    | (Selection.Scissors, Selection.Paper) -> 0
     | _ -> 3
 
-  outcome + calculateScoreForSelection mine
+  let (_, mine) = throw
+  outcome + (int)mine
 
-let calculateScore (guide: Throw seq) =
-  guide
-  |> Seq.map calculateScoreForThrow
-  |> Seq.sum
+module MistakenGuide =
+  let calculateScore (guide: Throw seq) =
+    guide
+    |> Seq.map calculateScoreForThrow
+    |> Seq.sum
 
-let guide =
-  File.ReadLines "Day2/input.txt"
+type Outcome = Win | Lose | Draw
+type Round = Selection * Outcome
+type ProperGuide = Round seq
+
+module ProperGuide =
+  let calculateScoreForRound round =
+    let throw =
+      match round with
+      | (Selection.Rock, Win) -> (Selection.Rock, Selection.Paper)
+      | (Selection.Rock, Draw) -> (Selection.Rock, Selection.Rock)
+      | (Selection.Rock, Lose) -> (Selection.Rock, Selection.Scissors)
+      | (Selection.Paper, Win) -> (Selection.Paper, Selection.Scissors)
+      | (Selection.Paper, Draw) -> (Selection.Paper, Selection.Paper)
+      | (Selection.Paper, Lose) -> (Selection.Paper, Selection.Rock)
+      | (Selection.Scissors, Win) -> (Selection.Scissors, Selection.Rock)
+      | (Selection.Scissors, Draw) -> (Selection.Scissors, Selection.Scissors)
+      | (Selection.Scissors, Lose) -> (Selection.Scissors, Selection.Paper)
+      | _ -> failwith "Shouldn't get here"
+    calculateScoreForThrow throw
+
+  let calculateScore (guide: Round seq) =
+    guide
+    |> Seq.map calculateScoreForRound
+    |> Seq.sum
+
+let fileName = "Day2/input.txt"
+
+let mistakenGuide: MistakenGuide =
+  File.ReadLines fileName
   |> Seq.map (fun s ->
     let split = s.Split " "
     let parsedOpponent =
       match split[0] with
-      | "A" -> Rock
-      | "B" -> Paper
-      | "C" -> Scissors
+      | "A" -> Selection.Rock
+      | "B" -> Selection.Paper
+      | "C" -> Selection.Scissors
       | _ -> failwith "Unknown selection"
 
     let parsedMine =
       match split[1] with
-      | "X" -> Rock
-      | "Y" -> Paper
-      | "Z" -> Scissors
+      | "X" -> Selection.Rock
+      | "Y" -> Selection.Paper
+      | "Z" -> Selection.Scissors
       | _ -> failwith "Unknown selection"
 
     (parsedOpponent, parsedMine)
   )
 
-printfn "Total score for guide: %d" (calculateScore guide)
+let properGuide: ProperGuide =
+  File.ReadLines fileName
+  |> Seq.map (fun s ->
+    let split = s.Split " "
+    let opponentSelection =
+      match split[0] with
+      | "A" -> Selection.Rock
+      | "B" -> Selection.Paper
+      | "C" -> Selection.Scissors
+      | _ -> failwith "Unknown selection"
+
+    let expectedOutCome =
+      match split[1] with
+      | "X" -> Lose
+      | "Y" -> Draw
+      | "Z" -> Win
+      | _ -> failwith "Unknown selection"
+
+    (opponentSelection, expectedOutCome)
+  )
+
+printfn "Total score for mistaken guide interpretation: %d" (MistakenGuide.calculateScore mistakenGuide)
+printfn "Total score for proper guide interpretation: %d" (ProperGuide.calculateScore properGuide)
