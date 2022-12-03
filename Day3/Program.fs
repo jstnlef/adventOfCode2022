082f9b64bceab823efc7dea768077604544b2709
Justin LeFebvre <jstnlefebvre@gmail.com>
2022-12-03 01:14:38 -0500

    Day3 part 1

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -1,4 +1,40 @@
-﻿open System.IO
+﻿open System
+open System.IO
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+type Rucksack = char Set * char Set
+
+let split size source =
+  seq {
+    let r = ResizeArray()
+    for x in source do
+      r.Add(x)
+      if r.Count = size then
+        yield r.ToArray()
+        r.Clear()
+    if r.Count > 0 then
+      yield r.ToArray()
+  }
+
+let fileName = "Day3/input.txt"
+
+let priorityForRucksack (rucksack: Rucksack) =
+  let (first, second) = rucksack
+  let shared = ((Set.intersect first second) |> Set.toArray)[0]
+  if (Char.IsUpper(shared)) then
+    int shared - int 'A' + 27
+  else
+    int shared - int 'a' + 1
+
+let sumOfPriorities rucksacks =
+  rucksacks
+  |> Seq.map priorityForRucksack
+  |> Seq.sum
+
+let rucksacks =
+  File.ReadLines fileName
+  |> Seq.map (fun line ->
+    let s = Seq.toArray (split (line.Length / 2) line)
+    (Set.ofArray s[0], Set.ofArray s[1])
+  )
+
+printfn "Sum of the rucksack priorities: %d" (sumOfPriorities rucksacks)
