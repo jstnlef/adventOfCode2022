f0f9761a1c6ec684b863fb2de7c25906155675a8
Justin LeFebvre <jstnlefebvre@gmail.com>
2022-12-03 02:11:08 -0500

    Code clean up

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -1,20 +1,6 @@
 ﻿open System
 open System.IO
 
-type Rucksack = char Set * char Set
-
-let split size source =
-  seq {
-    let r = ResizeArray()
-    for x in source do
-      r.Add(x)
-      if r.Count = size then
-        yield r.ToArray()
-        r.Clear()
-    if r.Count > 0 then
-      yield r.ToArray()
-  }
-
 let fileName = "Day3/input.txt"
 
 let priorityForChar c =
@@ -23,32 +9,28 @@ let priorityForChar c =
   else
     int c - int 'a' + 1
 
-let priorityForRucksack (rucksack: Rucksack) =
-  let (first, second) = rucksack
-  let shared = ((Set.intersect first second) |> Set.toArray)[0]
-  priorityForChar shared
+let priorityForRucksack rucksack =
+  let first, second =
+    rucksack
+    |> Array.splitAt (rucksack.Length / 2)
 
-let sumOfRucksackPriorities (rucksacks: Rucksack seq) =
+  Set.intersect (Set.ofArray first) (Set.ofArray second)
+  |> Seq.head
+  |> priorityForChar
+
+let sumOfRucksackPriorities rucksacks =
   rucksacks
   |> Seq.map priorityForRucksack
   |> Seq.sum
 
-let priorityFor3Elves (rucksacks: Rucksack array) =
-  let mergeSack rucksack =
-    let (a, b) = rucksack
-    Set.union a b
+let priorityFor3Elves rucksacks =
+  rucksacks
+  |> Seq.map Set.ofArray
+  |> Seq.reduce (fun first next -> Set.intersect first next)
+  |> Seq.head
+  |> priorityForChar
 
-  let shared =
-    rucksacks
-    |> Seq.map mergeSack
-    |> Seq.reduce (fun first next ->
-      Set.intersect first next
-    )
-    |> Seq.toArray
-
-  priorityForChar shared[0]
-
-let sumOf3ElfBadgePriorities (rucksacks: Rucksack seq) =
+let sumOf3ElfBadgePriorities rucksacks =
   rucksacks
   |> Seq.chunkBySize 3
   |> Seq.map priorityFor3Elves
@@ -56,10 +38,7 @@ let sumOf3ElfBadgePriorities (rucksacks: Rucksack seq) =
 
 let rucksacks =
   File.ReadLines fileName
-  |> Seq.map (fun line ->
-    let s = Seq.toArray (split (line.Length / 2) line)
-    (Set.ofArray s[0], Set.ofArray s[1])
-  )
+  |> Seq.map (fun line -> Array.ofSeq line)
 
 printfn "Sum of the shared rucksack priorities: %d" (sumOfRucksackPriorities rucksacks)
 printfn "Sum of the 3 elf group badge priorities: %d" (sumOf3ElfBadgePriorities rucksacks)
