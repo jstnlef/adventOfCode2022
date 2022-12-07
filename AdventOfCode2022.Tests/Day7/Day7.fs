c9e7558cf339e2ff21399fe73a0b7f37f3025d1a
Justin <jstnlefebvre@gmail.com>
2022-12-07 16:23:47 -0500

    Still not quite there

--- AdventOfCode2022.Tests/Day7/Day7.fs
+++ AdventOfCode2022.Tests/Day7/Day7.fs
@@ -9,9 +9,9 @@ open Day7
 let ``Sum of directories with total size less than 100000`` (fileName: string, expected: int) =
   let result =
     Shell.parseHistory fileName
-    |> Files.fromShellHistory
-    |> Files.getDirectories
-    |> Seq.map Files.calculateSize
+    |> FileSystem.fromShellHistory
+    |> FileSystem.getDirectories
+    |> Seq.map FileSystem.size
     |> Seq.filter (fun size -> size < 100000)
     |> Seq.sum
   Assert.Equal(expected, result)
@@ -27,45 +27,75 @@ let ``Part 2`` (fileName: string, expected: int) =
   let result = -1
   Assert.Equal(expected, result)
 
+[<Fact>]
+let ``parsing input creates ShellHistory`` () =
+  let expected : ShellHistory =
+    [
+      Command(ChangeDirectory("/"))
+      Command(ListDirectories)
+      FileEntry(FileEntry.Directory("a"))
+      FileEntry(FileEntry.File("b.txt", 14848514))
+      FileEntry(FileEntry.File("c.dat", 8504156))
+      FileEntry(FileEntry.Directory("d"))
+      Command(ChangeDirectory("a"))
+      Command(ListDirectories)
+      FileEntry(FileEntry.Directory("e"))
+      FileEntry(FileEntry.File("f", 29116))
+      FileEntry(FileEntry.File("g", 2557))
+      FileEntry(FileEntry.File("h.lst", 62596))
+      Command(ChangeDirectory("e"))
+      Command(ListDirectories)
+      FileEntry(FileEntry.File("i", 584))
+      Command(ChangeDirectory(".."))
+      Command(ChangeDirectory(".."))
+      Command(ChangeDirectory("d"))
+      Command(ListDirectories)
+      FileEntry(FileEntry.File("j", 4060174))
+      FileEntry(FileEntry.File("d.log", 8033020))
+      FileEntry(FileEntry.File("d.ext", 5626152))
+      FileEntry(FileEntry.File("k", 7214296))
+    ]
+  let result = Shell.parseHistory "Day7/sample.txt" |> Seq.toList
+  Assert.True((expected = result))
 
 [<Fact>]
 let ``calculateSize on file returns size`` () =
-  let file = Files.File(name = "test", size = 1099)
-  let size = Files.calculateSize file
+  let file = FileTree.File(name = "test", size = 1099)
+  let size = FileSystem.size file
   Assert.Equal(1099, size)
 
 [<Fact>]
 let ``calculateSize on directory recursively calculates size`` () =
-  let file1 = Files.File(name = "f1", size = 1)
-  let file2 = Files.File(name = "f2", size = 2)
-  let file3 = Files.File(name = "f3", size = 3)
-  let dir = Files.Directory(name = "d1", files = [file1; file2; file3])
-  let root = Files.Directory(name = "/", files = [dir; file3])
+  let file1 = FileTree.File(name = "f1", size = 1)
+  let file2 = FileTree.File(name = "f2", size = 2)
+  let file3 = FileTree.File(name = "f3", size = 3)
+  let dir = FileTree.Directory(name = "d1", files = [file1; file2; file3])
+  let root = FileTree.Directory(name = "/", files = [dir; file3])
 
-  let size = Files.calculateSize root
+  let size = FileSystem.size root
   Assert.Equal(9, size)
 
 [<Fact>]
 let ``calculateSize on entire filesystem`` () =
   let fileSystem =
-    Directory(name = "/", files = [
-      Directory(name = "a", files = [
-        Directory(name = "e", files = [
-          File(name= "i", size=584)
+    FileTree.Directory(name = "/", files = [
+      FileTree.Directory(name = "a", files = [
+        FileTree.Directory(name = "e", files = [
+          FileTree.File(name= "i", size=584)
         ])
-        File(name= "f", size=29116)
-        File(name= "g", size=2557)
-        File(name= "h.lst", size=62596)
+        FileTree.File(name= "f", size=29116)
+        FileTree.File(name= "g", size=2557)
+        FileTree.File(name= "h.lst", size=62596)
       ])
-      File(name= "b.txt", size=14848514)
-      File(name= "c.dat", size=8504156)
-      Directory(name = "d", files = [
-        File(name= "j", size=4060174)
-        File(name= "d.log", size=8033020)
-        File(name= "d.ext", size=5626152)
-        File(name= "k", size=7214296)
+      FileTree.File(name= "b.txt", size=14848514)
+      FileTree.File(name= "c.dat", size=8504156)
+      FileTree.Directory(name = "d", files = [
+        FileTree.File(name= "j", size=4060174)
+        FileTree.File(name= "d.log", size=8033020)
+        FileTree.File(name= "d.ext", size=5626152)
+        FileTree.File(name= "k", size=7214296)
       ])
     ])
 
-  let size = Files.calculateSize fileSystem
+  let size = FileSystem.size fileSystem
   Assert.Equal(48381165, size)
--- AdventOfCode2022/Day7.fs
+++ AdventOfCode2022/Day7.fs
@@ -1,57 +1,144 @@
 module Day7
 
-type Files =
-  | Directory of name: string * files: Files list
-  | File of name: string * size: int
+open System
 
 type Command =
-  | ChangeDirectory of string
+  | ChangeDirectory of name: string
   | ListDirectories
 
-type Entry =
-  | Command
-  // | File of File
+type FileEntry =
+  | Directory of name: string
+  | File of name: string * size: int
 
-type ShellHistory = Entry seq
+type ShellEntry =
+  | Command of Command
+  | FileEntry of FileEntry
 
-module Files =
-  let fromShellHistory history: Files =
-    Directory(name = "/", files = [
-      Directory(name = "a", files = [
-        Directory(name = "e", files = [
-          File(name= "i", size=584)
-        ])
-        File(name= "f", size=29116)
-        File(name= "g", size=2557)
-        File(name= "h.lst", size=62596)
-      ])
-      File(name= "b.txt", size=14848514)
-      File(name= "c.dat", size=8504156)
-      Directory(name = "d", files = [
-        File(name= "j", size=4060174)
-        File(name= "d.log", size=8033020)
-        File(name= "d.ext", size=5626152)
-        File(name= "k", size=7214296)
-      ])
-    ])
-
-  let rec getDirectories (files: Files): Files list =
-    let mutable dirs = []
-    match files with
-    | Directory(name, files) ->
-      if name <> "/" then
-        let d = Directory(name, files)
-        dirs <- d :: dirs
-
-      dirs <- dirs @ (List.collect getDirectories files)
-    | _ -> ()
-    dirs
-
-  let rec calculateSize (files: Files): int =
-    match files with
-    | Directory(files = files) -> files |> Seq.map calculateSize |> Seq.sum
-    | File(size = size) -> size
+type ShellHistory = ShellEntry seq
 
 module Shell =
+  open System.IO
+  open System.Text.RegularExpressions
+
+  let private commandRegex = Regex("\$ ([a-z]+) *([\w.\.\/]*)")
+  let private directoryRegex = Regex("dir ([\w.\.\/]*)")
+  let private fileRegex = Regex("(\d+) ([\w.\.\/]*)")
+
+  let private parseLine line: ShellEntry =
+    let commandMatch = commandRegex.Match line
+    let dirMatch = directoryRegex.Match line
+    let fileMatch = fileRegex.Match line
+    if commandMatch.Success then
+      if commandMatch.Groups[1].Value = "cd" then
+        Command(ChangeDirectory(commandMatch.Groups[2].Value))
+      else
+        Command(ListDirectories)
+    else if dirMatch.Success then
+      FileEntry(FileEntry.Directory(dirMatch.Groups[1].Value))
+    else
+      FileEntry(FileEntry.File(fileMatch.Groups[2].Value, int fileMatch.Groups[1].Value))
+
+
   let parseHistory filename: ShellHistory =
-    Seq.empty
+    File.ReadLines filename
+    |> Seq.map parseLine
+
+
+type FileTree =
+  | Directory of name: string * files: FileTree list
+  | File of name: string * size: int
+
+module FileSystem =
+  open System.Collections.Generic
+
+  let rec iter (file: FileTree): FileTree seq =
+    seq {
+      match file with
+      | Directory(_, children) as d ->
+        yield d
+        for f in children do
+          for child in iter f do
+            yield child
+      | File _ as f ->
+        yield f
+    }
+
+  let rec getDirectories (file: FileTree): FileTree seq =
+    let isDirectory file =
+      match file with Directory _ -> true | _ -> false
+
+    file
+    |> iter
+    |> Seq.filter isDirectory
+
+  let size (file: FileTree): int =
+    let sizeOfFile file =
+      match file with
+      | Directory _ -> 0
+      | File(size = size) -> size
+
+    file
+    |> iter
+    |> Seq.map sizeOfFile
+    |> Seq.sum
+
+  type private DiscoverState = {
+    fileTree: FileTree
+    breadCrumbs: Stack<FileTree>
+  }
+
+  let private entryToTree (entry: FileEntry): FileTree =
+    match entry with
+    | FileEntry.Directory name -> Directory(name, [])
+    | FileEntry.File(name, size) -> File(name, size)
+
+  let private addFile (entry: FileEntry) (fileTree: FileTree): FileTree =
+    match fileTree with
+    | FileTree.Directory(name, files) ->
+      FileTree.Directory(name, (entryToTree entry)::files)
+    | FileTree.File _ ->
+      failwith "Can't add a file to a file"
+
+  let findDirectoryByName (name: string) (fileTree: FileTree): bool =
+    match fileTree with
+    | FileTree.Directory(dName, _) when dName = name -> true
+    | _ -> false
+
+  let private replaceInTree (updatedFileTree: FileTree) (fileTree: FileTree): FileTree =
+    match updatedFileTree, fileTree with
+    | FileTree.Directory(updatedName, updatedFiles), FileTree.Directory(name, files) ->
+      let mutable filesArray =
+        files
+        |> List.toArray
+      let replaceIndex = filesArray |> Array.findIndex (findDirectoryByName updatedName)
+      filesArray[replaceIndex] <- updatedFileTree
+      FileTree.Directory(name, Array.toList filesArray)
+    | _ -> failwith "shouldn't work with files"
+
+  let private processCommand (state: DiscoverState) (command: Command): DiscoverState =
+    match command with
+    | Command.ChangeDirectory name when name = ".." ->
+      let parent = state.breadCrumbs.Pop()
+      { state with fileTree = replaceInTree state.fileTree parent }
+    | Command.ChangeDirectory name ->
+      state.breadCrumbs.Push(state.fileTree)
+      { state with fileTree = state.fileTree |> iter |> Seq.find (findDirectoryByName name) }
+    | Command.ListDirectories -> state
+
+  let private discover state (entry: ShellEntry): DiscoverState =
+    match entry with
+    | FileEntry f ->
+      {state with fileTree = addFile f state.fileTree}
+    | Command c ->
+      processCommand state c
+
+  let fromShellHistory (history: ShellHistory): FileTree =
+    let undiscoveredTree = FileTree.Directory(name = "/", files = [])
+    let state = {
+      fileTree = undiscoveredTree
+      breadCrumbs = Stack()
+    }
+    let mutable state = Seq.fold discover state (history |> Seq.tail)
+    while state.breadCrumbs.Count > 0 do
+      state <- discover state (ShellEntry.Command(Command.ChangeDirectory("..")))
+    state.fileTree
