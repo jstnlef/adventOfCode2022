b6885f96abdc20d1319a3bd639d8c740f89825ca
Justin LeFebvre <jstnlefebvre@gmail.com>
2022-12-07 02:44:15 -0500

    man part 7 is quite a bit of a jump

--- AdventOfCode2022.Tests/Day7/Day7.fs
+++ AdventOfCode2022.Tests/Day7/Day7.fs
@@ -4,23 +4,68 @@ open Xunit
 open Day7
 
 [<Theory>]
-[<InlineData("Day7/sample.txt", "")>]
-[<InlineData("Day7/input.txt", "")>]
-let ``Part 1`` (fileName: string, expected: string) =
-  // let result =
-  //   parseSuppliesAndProcedures fileName
-  //   ||> Supplies.rearrange Step.performWithCrateMover9000
-  //   |> Supplies.topCratesPerStack
-  let result = ""
+[<InlineData("Day7/sample.txt", 95437)>]
+[<InlineData("Day7/input.txt", -1)>]
+let ``Sum of directories with total size less than 100000`` (fileName: string, expected: int) =
+  let result =
+    Shell.parseHistory fileName
+    |> Files.fromShellHistory
+    |> Files.getDirectories
+    |> Seq.map Files.calculateSize
+    |> Seq.filter (fun size -> size < 100000)
+    |> Seq.sum
   Assert.Equal(expected, result)
 
 [<Theory>]
-[<InlineData("Day7/sample.txt", "")>]
-[<InlineData("Day7/input.txt", "")>]
-let ``Part 2`` (fileName: string, expected: string) =
+[<InlineData("Day7/sample.txt", -1)>]
+[<InlineData("Day7/input.txt", -1)>]
+let ``Part 2`` (fileName: string, expected: int) =
   // let result =
   //   parseSuppliesAndProcedures fileName
   //   ||> Supplies.rearrange Step.performWithCrateMover9000
   //   |> Supplies.topCratesPerStack
-  let result = ""
+  let result = -1
   Assert.Equal(expected, result)
+
+
+[<Fact>]
+let ``calculateSize on file returns size`` () =
+  let file = Files.File(name = "test", size = 1099)
+  let size = Files.calculateSize file
+  Assert.Equal(1099, size)
+
+[<Fact>]
+let ``calculateSize on directory recursively calculates size`` () =
+  let file1 = Files.File(name = "f1", size = 1)
+  let file2 = Files.File(name = "f2", size = 2)
+  let file3 = Files.File(name = "f3", size = 3)
+  let dir = Files.Directory(name = "d1", files = [file1; file2; file3])
+  let root = Files.Directory(name = "/", files = [dir; file3])
+
+  let size = Files.calculateSize root
+  Assert.Equal(9, size)
+
+[<Fact>]
+let ``calculateSize on entire filesystem`` () =
+  let fileSystem =
+    Directory(name = "/", files = [
+      Directory(name = "a", files = [
+        Directory(name = "e", files = [
+          File(name= "i", size=584)
+        ])
+        File(name= "f", size=29116)
+        File(name= "g", size=2557)
+        File(name= "h.lst", size=62596)
+      ])
+      File(name= "b.txt", size=14848514)
+      File(name= "c.dat", size=8504156)
+      Directory(name = "d", files = [
+        File(name= "j", size=4060174)
+        File(name= "d.log", size=8033020)
+        File(name= "d.ext", size=5626152)
+        File(name= "k", size=7214296)
+      ])
+    ])
+
+  let size = Files.calculateSize fileSystem
+  Assert.Equal(48381165, size)
--- AdventOfCode2022/Day7.fs
+++ AdventOfCode2022/Day7.fs
@@ -1,3 +1,57 @@
 module Day7
 
-open System.IO
+type Files =
+  | Directory of name: string * files: Files list
+  | File of name: string * size: int
+
+type Command =
+  | ChangeDirectory of string
+  | ListDirectories
+
+type Entry =
+  | Command
+  // | File of File
+
+type ShellHistory = Entry seq
+
+module Files =
+  let fromShellHistory history: Files =
+    Directory(name = "/", files = [
+      Directory(name = "a", files = [
+        Directory(name = "e", files = [
+          File(name= "i", size=584)
+        ])
+        File(name= "f", size=29116)
+        File(name= "g", size=2557)
+        File(name= "h.lst", size=62596)
+      ])
+      File(name= "b.txt", size=14848514)
+      File(name= "c.dat", size=8504156)
+      Directory(name = "d", files = [
+        File(name= "j", size=4060174)
+        File(name= "d.log", size=8033020)
+        File(name= "d.ext", size=5626152)
+        File(name= "k", size=7214296)
+      ])
+    ])
+
+  let rec getDirectories (files: Files): Files list =
+    let mutable dirs = []
+    match files with
+    | Directory(name, files) ->
+      if name <> "/" then
+        let d = Directory(name, files)
+        dirs <- d :: dirs
+
+      dirs <- dirs @ (List.collect getDirectories files)
+    | _ -> ()
+    dirs
+
+  let rec calculateSize (files: Files): int =
+    match files with
+    | Directory(files = files) -> files |> Seq.map calculateSize |> Seq.sum
+    | File(size = size) -> size
+
+module Shell =
+  let parseHistory filename: ShellHistory =
+    Seq.empty
