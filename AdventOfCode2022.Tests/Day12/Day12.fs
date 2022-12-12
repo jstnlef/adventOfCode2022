258b7ba2c0f71dc8f76c1fd6bf7d19b436a9894a
Justin LeFebvre <jstnlefebvre@gmail.com>
2022-12-12 00:59:52 -0500

    Got the parsing for day 12 done

--- AdventOfCode2022.Tests/Day12/Day12.fs
+++ AdventOfCode2022.Tests/Day12/Day12.fs
@@ -1,12 +1,13 @@
 module Tests.Day12
 
+open Day12
 open Xunit
 
 [<Theory>]
-[<InlineData("Day12/sample.txt", -1)>]
+[<InlineData("Day12/sample.txt", 31)>]
 [<InlineData("Day12/input.txt", -1)>]
-let ``Part 1`` (fileName: string, expected: int) =
-  let result = -1
+let ``Fewest steps required to move from current position to best signal`` (fileName: string, expected: int) =
+  let result = HeightMap.parse fileName |> HeightMap.shortestStepsToGoal
 
   Assert.Equal(expected, result)
 
--- AdventOfCode2022/Day12.fs
+++ AdventOfCode2022/Day12.fs
@@ -1,1 +1,27 @@
 module Day12
+
+open System.IO
+
+type Position =
+  | Height of int
+  | Start
+  | BestSignal
+
+type HeightMap = Position array array
+
+module HeightMap =
+  let shortestStepsToGoal heightmap : int =
+    // TODO: Should be pretty easy to to implement A* or breadth first search to get this done. Just no time now.
+    0
+
+  let parse filename : HeightMap =
+    let parseChar c =
+      match c with
+      | 'S' -> Start
+      | 'E' -> BestSignal
+      | c -> Height(int c - int 'a')
+
+    let parseLine (line: string) =
+      line |> Seq.map parseChar |> Seq.toArray
+
+    File.ReadLines filename |> Seq.map parseLine |> Seq.toArray
