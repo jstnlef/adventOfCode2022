46c973f8b6081cd08d651556daf9fc6577068c92
Justin <jstnlefebvre@gmail.com>
2022-12-13 15:16:45 -0500

    Attempt to implement comparison algorithm

--- AdventOfCode2022.Tests/Day13/Day13.fs
+++ AdventOfCode2022.Tests/Day13/Day13.fs
@@ -1,12 +1,14 @@
 module Tests.Day13
 
+open Day13
 open Xunit
 
 [<Theory>]
-[<InlineData("Day13/sample.txt", -1)>]
+[<InlineData("Day13/sample.txt", 13)>]
 [<InlineData("Day13/input.txt", -1)>]
-let ``Part 1`` (fileName: string, expected: int) =
-  let result = -1
+let ``Sum of the indices of the correct pairs`` (fileName: string, expected: int) =
+  let result =
+    DistressSignal.parse fileName |> DistressSignal.findCorrectPairs |> Seq.sum
 
   Assert.Equal(expected, result)
 
@@ -17,3 +19,60 @@ let ``Part 2`` (fileName: string, expected: int) =
   let result = -1
 
   Assert.Equal(expected, result)
+
+[<Theory>]
+[<InlineData(0, true)>]
+[<InlineData(1, true)>]
+[<InlineData(2, false)>]
+[<InlineData(3, true)>]
+[<InlineData(4, false)>]
+[<InlineData(5, true)>]
+[<InlineData(6, false)>]
+[<InlineData(7, false)>]
+let ``Test in correct order`` (index: int, expected: bool) =
+  let tests =
+    [| List([| Integer(1); Integer(1); Integer(3); Integer(1); Integer(1) |]),
+       List([| Integer(1); Integer(1); Integer(5); Integer(1); Integer(1) |])
+
+       List([| List([| Integer(1) |]); List([| Integer(2); Integer(3); Integer(4) |]) |]),
+       List([| List([| Integer(1) |]); Integer(4) |])
+
+       List([| Integer(9) |]), List([| List([| Integer(8); Integer(7); Integer(6) |]) |])
+
+       List([| List([| Integer(4); Integer(4) |]); Integer(4); Integer(4) |]),
+       List([| List([| Integer(4); Integer(4) |]); Integer(4); Integer(4); Integer(4) |])
+
+       List([| Integer(7); Integer(7); Integer(7); Integer(7) |]), List([| Integer(7); Integer(7); Integer(7) |])
+
+       List([||]), List([| Integer(3) |])
+
+       List([| List([| List [||] |]) |]), List([| List([||]) |])
+
+       List(
+         [| Integer(1)
+            List(
+              [| Integer(2)
+                 List(
+                   [| Integer(3)
+                      List([| Integer(4); List([| Integer(5); Integer(6); Integer(7) |]) |]) |]
+                 ) |]
+            )
+            Integer(8)
+            Integer(9) |]
+       ),
+       List(
+         [| Integer(1)
+            List(
+              [| Integer(2)
+                 List(
+                   [| Integer(3)
+                      List([| Integer(4); List([| Integer(5); Integer(6); Integer(0) |]) |]) |]
+                 ) |]
+            )
+            Integer(8)
+            Integer(9) |]
+       ) |]
+
+  let isCorrect = tests[index] |> PacketPair.isInCorrectOrder
+
+  Assert.Equal(expected, isCorrect)
--- AdventOfCode2022/Day13.fs
+++ AdventOfCode2022/Day13.fs
@@ -1,12 +1,50 @@
 module Day13
 
+open System.IO
+
 type Packet =
-  | Numbers of int
-  | List of Packet list
+  | Integer of int
+  | List of Packet array
 
-type PacketPair = { left: Packet; right: Packet }
+type PacketPair = Packet * Packet
 
-type DistressSignal = PacketPair seq
+module PacketPair =
+  let rec isInCorrectOrder (left, right) : bool =
+    let compareIntegers left right : bool = left <= right
+
+    let compareLists (left: Packet array) (right: Packet array) : bool =
+      let mutable i = 0
+      let mutable state = true
+
+      while (state && i < left.Length && i <= right.Length) do
+        if i >= right.Length then
+          state <- false
+        else
+          state <- isInCorrectOrder (left[i], right[i])
+
+        i <- i + 1
+
+      state
+
+    match (left, right) with
+    | Integer l, Integer r -> compareIntegers l r
+    | List l, List r -> compareLists l r
+    | Integer l, List r -> compareLists [| Integer(l) |] r
+    | List l, Integer r -> compareLists l [| Integer(r) |]
+
+type DistressSignal = PacketPair array
 
 module DistressSignal =
-  let parse filename = Seq.empty
+  let findCorrectPairs (distressSignal: DistressSignal) : int seq =
+    distressSignal
+    |> Seq.filter PacketPair.isInCorrectOrder
+    |> Seq.mapi (fun i _ -> i + 1)
+
+  let parse filename : DistressSignal =
+    let parseLine line = Integer 0
+
+    File.ReadAllText filename
+    |> (fun s -> s.Split("\n\n"))
+    |> Array.map (fun s ->
+      let split = s.Split("\n")
+      parseLine split[0], parseLine split[1])
