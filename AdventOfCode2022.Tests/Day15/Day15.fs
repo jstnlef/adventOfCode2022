9c882f84c9b520f861c87fbcec39886722c9a57c
Justin <jstnlefebvre@gmail.com>
2022-12-15 12:26:15 -0500

    stub day 15

---
+++ AdventOfCode2022.Tests/Day15/Day15.fs
@@ -0,0 +1,17 @@
+module Tests.Day15
+
+open Xunit
+
+[<Theory>]
+[<InlineData("Day15/sample.txt", -1)>]
+[<InlineData("Day15/input.txt", -1)>]
+let ``Part 1`` (fileName: string, expected: int) =
+  let result = -1
+  Assert.Equal(expected, result)
+
+[<Theory>]
+[<InlineData("Day15/sample.txt", -1)>]
+[<InlineData("Day15/input.txt", -1)>]
+let ``Part 2`` (fileName: string, expected: int) =
+  let result = -1
+  Assert.Equal(expected, result)
