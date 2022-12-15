8b67048b8026a2f3f46e476ca28359cca5336735
Justin <jstnlefebvre@gmail.com>
2022-12-15 14:48:17 -0500

    started day 15

--- AdventOfCode2022.Tests/Day15/Day15.fs
+++ AdventOfCode2022.Tests/Day15/Day15.fs
@@ -1,17 +1,20 @@
 module Tests.Day15
 
+open Day15
 open Xunit
+open Xunit.Abstractions
 
-[<Theory>]
-[<InlineData("Day15/sample.txt", -1)>]
-[<InlineData("Day15/input.txt", -1)>]
-let ``Part 1`` (fileName: string, expected: int) =
-  let result = -1
-  Assert.Equal(expected, result)
+type Day15(output: ITestOutputHelper) =
+  [<Theory>]
+  [<InlineData("Day15/sample.txt", 10, 26)>]
+  [<InlineData("Day15/input.txt", 2000000, -1)>]
+  member _.``Find the number of positions which cannot contain a beacon``(fileName: string, row: int, expected: int) =
+    let result = SensorReport.parse fileName |> Tunnels.positionsWithoutBeacon row
+    Assert.Equal(expected, result)
 
-[<Theory>]
-[<InlineData("Day15/sample.txt", -1)>]
-[<InlineData("Day15/input.txt", -1)>]
-let ``Part 2`` (fileName: string, expected: int) =
-  let result = -1
-  Assert.Equal(expected, result)
+  [<Theory>]
+  [<InlineData("Day15/sample.txt", -1)>]
+  [<InlineData("Day15/input.txt", -1)>]
+  member _.``Part 2``(fileName: string, expected: int) =
+    let result = -1
+    Assert.Equal(expected, result)
--- AdventOfCode2022/Day15.fs
+++ AdventOfCode2022/Day15.fs
@@ -1,1 +1,57 @@
 module Day15
+
+type SensorReportEntry =
+  { sensor: int * int; beacon: int * int }
+
+type SensorReport = SensorReportEntry seq
+
+module SensorReport =
+  open System.IO
+  open System.Text.RegularExpressions
+
+  let entryRegex =
+    Regex(
+      "Sensor at x=(?<sensorX>-?\d+), y=(?<sensorY>-?\d+): closest beacon is at x=(?<beaconX>-?\d+), y=(?<beaconY>-?\d+)"
+    )
+
+  let parse filename : SensorReport =
+    let parseLine line =
+      let m = entryRegex.Match(line)
+
+      { sensor = int m.Groups["sensorX"].Value, int m.Groups["sensorY"].Value
+        beacon = int m.Groups["beaconX"].Value, int m.Groups["beaconY"].Value }
+
+    File.ReadLines filename |> Seq.map parseLine
+
+type Location =
+  | Sensor
+  | Beacon
+  | NoBeacon
+  | Unknown
+
+type Map = Location array array
+
+let manhattanDistance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+
+module Tunnels =
+  let generateMap row report : Map =
+    // Run a flood fill on the tunnels and then select the entries which are `NoBeacon`
+
+    Array.empty
+
+  let positionsWithoutBeacon (row: int) (report: SensorReport) : int =
+    let mapForRow = generateMap row report
+
+    -1
+
+// let render tunnels : string =
+//   let renderLocation location =
+//     match location with
+//     | Sensor -> "S"
+//     | Beacon -> "B"
+//     | NoBeacon -> "#"
+//     | Unknown -> "."
+//
+//   tunnels.map
+//   |> Array.map (fun row -> String.concat "" (row |> Array.map renderLocation))
+//   |> String.concat "\n"
