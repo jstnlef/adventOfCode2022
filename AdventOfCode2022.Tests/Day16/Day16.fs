59ee0b11093312b47e0d02757bf87494d970a77e
Justin LeFebvre <jstnlefebvre@gmail.com>
2022-12-16 00:44:22 -0500

    Parsing the input for day 16

--- AdventOfCode2022.Tests/Day16/Day16.fs
+++ AdventOfCode2022.Tests/Day16/Day16.fs
@@ -1,12 +1,13 @@
 module Tests.Day16
 
+open Day16
 open Xunit
 
 [<Theory>]
-[<InlineData("Day16/sample.txt", -1)>]
+[<InlineData("Day16/sample.txt", 1651)>]
 [<InlineData("Day16/input.txt", -1)>]
-let ``Part 1`` (fileName: string, expected: int) =
-  let result = - 1
+let ``What is the most pressure you can release in 30 minutes?`` (fileName: string, expected: int) =
+  let result = Pipes.parse fileName |> Pipes.findMostPressureReleased 30
   Assert.Equal(expected, result)
 
 [<Theory>]
--- AdventOfCode2022/Day16.fs
+++ AdventOfCode2022/Day16.fs
@@ -1,1 +1,28 @@
 module Day16
+
+open System.Text.RegularExpressions
+
+type Pipe =
+  { id: string
+    flowRate: int
+    valves: string array }
+
+type Pipes = Pipe seq
+
+module Pipes =
+  open System.IO
+
+  let findMostPressureReleased timeframe pipes = -1
+
+  let regex =
+    Regex("Valve (?<id>\w\w) has flow rate=(?<flowRate>\d+); tunnels? leads? to valves? (?<valves>.*)")
+
+  let parse filename =
+    let parseLine line =
+      let m = regex.Match(line)
+
+      { id = m.Groups["id"].Value
+        flowRate = int m.Groups["flowRate"].Value
+        valves = m.Groups[ "valves" ].Value.Split(",") }
+
+    File.ReadLines filename |> Seq.map parseLine
